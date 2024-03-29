fd7e9d92c72687c93a583645fbe50827757a3685
unknown <pedromanmelo@gmail.com>
2024-03-29 17:26:39 -0300

    add folder

---
+++ ProjetoComposer/bootstrap.php
@@ -0,0 +1,62 @@
+<?php
+
+require __DIR__."/vendor/autoload.php";
+
+$metodo = $_SERVER['REQUEST_METHOD'];
+$caminho = $_SERVER['PATH_INFO'] ?? '/';
+
+$r = new Php\Primeiroprojeto\Router($metodo, $caminho);
+
+#ROTAS
+
+$r->get('/olamundo', function (){
+    return "Olá mundo!";
+} );
+
+$r->get('/olapessoa/{nome}', function($params){
+    return 'Olá '.$params[1];
+} );
+
+$r->get('/exer1/formulario', function(){
+    include("exer1.html");
+} );
+
+$r->post('/exer1/resposta', function(){
+    $valor1 = $_POST['valor1'];
+    $valor2 = $_POST['valor2'];
+    $soma = $valor1 + $valor2;
+    return "a soma é :" . $soma;
+} );
+
+# Lista 
+
+$r->get('/ex1/formulario', function(){
+    include("ex1.html");
+} );
+
+$r->post('/ex1/resposta', function(){
+    $valor1 = $_POST['valor1'];
+    
+    if ($valor1 > 0) {
+        return "Valor positivo";
+    } else if ($valor1 == 0) {
+        return "Valor igual a zero";
+    } else {
+        return "Valor negativo";
+    }
+    
+} );
+
+#ROTAS
+
+$resultado = $r->handler();
+
+if(!$resultado){
+    http_response_code(404);
+    echo "Página não encontrada!";
+    die();
+}
+
+echo $resultado($r->getParams());
+
+
