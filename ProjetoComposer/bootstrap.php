2941683b8ac8795bf5e47644c5d93922e5122890
unknown <pedromanmelo@gmail.com>
2024-03-29 18:51:37 -0300

    ex 04 ok

--- ProjetoComposer/bootstrap.php
+++ ProjetoComposer/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__."/vendor/autoload.php";
+require __DIR__ . "/vendor/autoload.php";
 
 $metodo = $_SERVER['REQUEST_METHOD'];
 $caminho = $_SERVER['PATH_INFO'] ?? '/';
@@ -9,34 +9,34 @@ $r = new Php\Primeiroprojeto\Router($metodo, $caminho);
 
 #ROTAS
 
-$r->get('/olamundo', function (){
+$r->get('/olamundo', function () {
     return "Olá mundo!";
-} );
+});
 
-$r->get('/olapessoa/{nome}', function($params){
-    return 'Olá '.$params[1];
-} );
+$r->get('/olapessoa/{nome}', function ($params) {
+    return 'Olá ' . $params[1];
+});
 
-$r->get('/exer1/formulario', function(){
-    include("exer1.html");
-} );
+$r->get('/exer1/formulario', function () {
+    include ("exer1.html");
+});
 
-$r->post('/exer1/resposta', function(){
+$r->post('/exer1/resposta', function () {
     $valor1 = $_POST['valor1'];
     $valor2 = $_POST['valor2'];
     $soma = $valor1 + $valor2;
     return "a soma é :" . $soma;
-} );
+});
 
 # Lista 
 
-$r->get('/ex1/formulario', function(){
-    include("ex1.html");
-} );
+$r->get('/ex1/formulario', function () {
+    include ("ex1.html");
+});
 
-$r->post('/ex1/resposta', function(){
+$r->post('/ex1/resposta', function () {
     $valor1 = $_POST['valor1'];
-    
+
     if ($valor1 > 0) {
         return "Valor positivo";
     } else if ($valor1 == 0) {
@@ -44,34 +44,55 @@ $r->post('/ex1/resposta', function(){
     } else {
         return "Valor negativo";
     }
-    
-} );
+
+});
 
 
-$r->get('/ex3/formulario', function(){
-    include("ex3.html");
-} );
+$r->get('/ex3/formulario', function () {
+    include ("ex3.html");
+});
 
-$r->post('/ex3/resposta', function(){
+$r->post('/ex3/resposta', function () {
     $valor1 = $_POST['valor1'];
     $valor2 = $_POST['valor2'];
-    
+
     $soma = $valor1 + $valor2;
 
-    if ($valor1 == $valor2){
+    if ($valor1 == $valor2) {
         return "Os valores são iguais: " . $soma * 3;
     } else {
         return "$valor1 + $valor2 = " . $soma;
     }
+});
+
+$r->get('/ex4/formulario', function () {
+    include ("ex4.html");
+});
+
+$r->post('/ex4/resposta', function () {
+    $valor1 = $_POST['valor1'];
+
     
-    
-} );
+    function tabuada($valor)
+    {
+        $i = 1;
+        while ($i <= 10) {
+
+            echo "<p>$valor x $i = " . $valor * $i . "</p>";
+
+            $i++;
+        }
+    }
+
+    return tabuada($valor1);
+
+});
 
 #ROTAS
 
 $resultado = $r->handler();
 
-if(!$resultado){
+if (!$resultado) {
     http_response_code(404);
     echo "Página não encontrada!";
     die();
